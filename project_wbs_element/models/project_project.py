ed785c6c3057b9873ae1a392b76d3849a49e2808
ougc27 <oscar_garzacordova@hotmail.com>
2016-11-11 09:20:10 -0600

    [DEL][project_wbs_element] - Percentages fields.

--- project_wbs_element/models/project_project.py
+++ project_wbs_element/models/project_project.py
@@ -12,12 +12,14 @@ class ProjectProject(models.Model):
     wbs_element_ids = fields.One2many(
         comodel_name='project.wbs_element',
         inverse_name='project_id',
-        copy=True)
-    nbr_wbs_elements = fields.Integer(
-        'Number of WBS Elements',
-        compute='_compute_count_wbs_elements')
-    label_tasks = fields.Char(
-        default='Concepts')
+        copy=True
+    )
+    nbr_wbs_elements = fields.Integer('Number of WBS Elements',
+                                      compute='_compute_count_wbs_elements')
+    label_tasks = fields.Char(default='Concepts')
+    indirects_analytic_account_id = fields.Many2one(
+        'account.analytic.account',
+        string='Indirects Analytic Account')
 
     @api.depends('wbs_element_ids')
     def _compute_count_wbs_elements(self):
