e6ef34c161ad064e1a73f61fa8d1db254430dcbc
lmgr0312 <lmiguel.gruiz@gmail.com>
2017-08-18 11:41:55 -0500

    [Validations for UOM, Products & Customers]

--- opus_to_odoo/wizards/import_opus_wizard.py
+++ opus_to_odoo/wizards/import_opus_wizard.py
@@ -8,6 +8,9 @@ from openerp.exceptions import UserError
 from os import getenv
 import pymssql
 import base64
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 class ImportOpusWizard(models.TransientModel):
@@ -38,7 +41,7 @@ class ImportOpusWizard(models.TransientModel):
                     server=server, user=username,
                     password=password, database=database)
             except:
-                raise UserError('hola')
+                raise UserError(_('Opus Server Not Found'))
         return conn
 
     @api.model
@@ -67,10 +70,48 @@ class ImportOpusWizard(models.TransientModel):
             WHERE nr.Alias = 'Material'"""))
         uom_names = cr.fetchall()
         uom_obj = self.env['product.uom']
+        length = 0
+        for uom in uom_names:
+            if length < len(uom[0]):
+                length = len(uom[0])
         for uom in uom_names:
             if not uom_obj.search([('name', '=', uom[0])]):
-                missing_uom.append(_('%s not exist' % uom[0].ljust(8)))
+                missing_uom.append(_('%s not exist' % uom[0].ljust(length+5)))
         return missing_uom
+
+    @api.model
+    def validate_products(self, cr):
+        missing_products = []
+        cr.execute(
+            str("""SELECT Recurso.Clave FROM Recurso
+            INNER JOIN NaturalezaDeRecurso
+                ON Recurso.NaturalezaDeRecursoId = NaturalezaDeRecurso.NaturalezaDeRecursoId
+            WHERE NaturalezaDeRecurso.Alias = 'Material'"""))
+        products_codes = cr.fetchall()
+        prod_obj = self.env['product.product']
+        length = 0
+        for code in products_codes:
+            if length < len(code[0]):
+                length = len(code[0])
+        for code in products_codes:
+            if not prod_obj.search([('default_code', '=', code[0])]):
+                missing_products.append(_('%s not exist' % code[0].ljust(length+5)))
+        return missing_products
+
+    @api.model
+    def validate_customers(self, cr):
+        missing_customers = []
+        cr.execute("SELECT ClienteId, Nombre FROM Cliente")
+        customers = cr.fetchall()
+        cust_obj = self.env['res.partner']
+        length = 0
+        for cust in customers:
+            if length < len(cust[1]):
+                length = len(cust[1])
+        for customer in customers:
+            if not cust_obj.search([('name', '=', customer[1])]):
+                missing_customers.append(_('%s not exist' % customer[1].ljust(length+5)))
+        return missing_customers
 
     @api.multi
     def validate_project(self):
@@ -83,6 +124,16 @@ class ImportOpusWizard(models.TransientModel):
             if missing_uom:
                 log_data += _("   Missing UoM \n" + ("-"*18) + "\n")
                 for error in missing_uom:
+                    log_data += (error + "\n")
+            missing_products = self.validate_products(cr)
+            if missing_products:
+                log_data += _("   Missing Products \n" + ("-"*18) + "\n")
+                for error in missing_products:
+                    log_data += (error + "\n")
+            missing_customers = self.validate_customers(cr)
+            if missing_customers:
+                log_data += _("   Missing Customers \n" + ("-"*18) + "\n")
+                for error in missing_customers:
                     log_data += (error + "\n")
             if len(log_data) > 0:
                 self.log_binary = base64.b64encode(log_data)
